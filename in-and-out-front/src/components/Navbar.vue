62810ad0126126434c1cd2d46f31d0ddca71ba49
h-e-l-e-n <leeleilei07@gmail.com>
2025-02-03 16:55:38 +0800

    layout: navbar

--- in-and-out-front/src/components/Navbar.vue
+++ in-and-out-front/src/components/Navbar.vue
@@ -3,15 +3,23 @@
 </script>
 
 <template>
-  <BNavbar style="background-color: #748BA7;">
-    <h3 class="mb-0">💰收益系統</h3>
+  <BNavbar style="background-color: #D9C3A5;">
+    <h3 class="mb-0" style="color: #6F5E4D;">💰賣家小世界</h3>
       <BNavbarNav>
-        <BButton pill variant="primary">進貨成本</BButton>
+        <BButton pill class="nav-buttons" style="background-color: #EED19C;">進貨</BButton>
+        <BButton pill class="nav-buttons" style="background-color: #EFB28C;">庫存</BButton>
+        <BButton pill class="nav-buttons" style="background-color: #E8837E;">訂單</BButton>
         <BNavItemDropdown text="User" right>
-          <BDropdownItem href="#">Account</BDropdownItem>
-          <BDropdownItem href="#">Settings</BDropdownItem>
+          <BDropdownItem href="#">Login</BDropdownItem>
         </BNavItemDropdown>
       </BNavbarNav>
     </BNavbar>
 </template>
+<style scoped>
+.nav-buttons {
+  border: 0;
+  color: #5C4033;
+  margin-right: 1rem;
+}
 
+</style>

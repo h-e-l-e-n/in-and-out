3ee4201e5fd3b9240ad92a353c0421b54ec84a43
h-e-l-e-n <leeleilei07@gmail.com>
2025-02-05 23:17:01 +0800

    feat: navbar router

--- in-and-out-front/src/components/Navbar.vue
+++ in-and-out-front/src/components/Navbar.vue
@@ -1,14 +1,31 @@
 <script setup>
+import router from '@/router';
+
 
 </script>
 
 <template>
   <BNavbar style="background-color: #D9C3A5;">
-    <h3 class="mb-0" style="color: #6F5E4D;">💰賣家小世界</h3>
+    <h3 class="mb-0" style="color: #6F5E4D;">💰招財進寶</h3>
       <BNavbarNav>
-        <BButton pill class="nav-buttons" style="background-color: #EED19C;">進貨</BButton>
-        <BButton pill class="nav-buttons" style="background-color: #EFB28C;">庫存</BButton>
-        <BButton pill class="nav-buttons" style="background-color: #E8837E;">訂單</BButton>
+        <BButton
+        pill
+        class="nav-buttons"
+        style="background-color: #EED19C;"
+        @click="router.push('/')"
+        >進貨</BButton>
+        <BButton
+        pill
+        class="nav-buttons"
+        style="background-color: #EFB28C;"
+        @click="router.push('/stock')"
+        >庫存</BButton>
+        <BButton
+        pill
+        class="nav-buttons"
+        style="background-color: #E8837E;"
+        @click="router.push('/order')"
+        >訂單</BButton>
         <BNavItemDropdown text="User" right>
           <BDropdownItem href="#">Login</BDropdownItem>
         </BNavItemDropdown>

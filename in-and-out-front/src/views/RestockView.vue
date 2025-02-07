4da3cf995431811fe0aad46e6a18a6873186032e
h-e-l-e-n <leeleilei07@gmail.com>
2025-02-07 23:57:27 +0800

    fix: RWD

--- in-and-out-front/src/views/RestockView.vue
+++ in-and-out-front/src/views/RestockView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, ref, computed } from 'vue'
+import { onMounted, ref } from 'vue'
 import { getRestockRecordAPI, addNewRecordAPI } from '../../apis/restockAPIs'
 import { formatDate, formatDateTime } from '@/utils/dayjsDate'
 
@@ -51,34 +51,32 @@ onMounted(() => {
     <div class="restock">
       <BButton v-b-toggle.collapse-1 class="m-1 mb-4 restock-btn">+ 進貨</BButton>
       <BCollapse id="collapse-1">
-        <div class="row" style="width: 80%;">
-          <div class="col">
+        <div class="row add-stock-area">
+          <div class="col-md-4 col col-xl-2">
             <input v-model="newStockRecord.product_name" type="text" class="form-control" placeholder="商品名稱" aria-label="Product name">
           </div>
-          <div class="col">
+          <div class="col-md-4 col col-xl-1">
             <input v-model="newStockRecord.amount" type="number" class="form-control" placeholder="數量">
           </div>
-          <div class="col">
+          <div class="col-md-4 col col-xl-2">
             <input v-model="newStockRecord.purchase_cost" type="text" class="form-control" placeholder="進貨成本">
           </div>
           <div class="col">
             <input v-model="newStockRecord.expire_date" type="date" class="form-control" placeholder="有效期限">
           </div>
           <div class="col">
-            <!-- <input type="text" class="form-control" placeholder="貨源"> -->
-            <!-- <label for="inputState" class="form-label">State</label> -->
             <select v-model="newStockRecord.product_source" id="inputState" class="form-select" style="color: #515151;">
               <option value="" disabled>貨源</option>
               <option v-for="option in sourceOptions" :key="option.id" :value="option.name">
                 {{ option.name }}
-              </option>            
+              </option>
             </select>
           </div>
           <div class="col">
             <input v-model="newStockRecord.purchase_order_number" type="text" class="form-control" placeholder="進貨單號">
           </div>
           <div class="col">
-            <BButton class="buttons" @click="handleNewStock">新增</BButton>
+            <BButton class="button" @click="handleNewStock">新增</BButton>
           </div>
         </div>
       </BCollapse>
@@ -90,7 +88,7 @@ onMounted(() => {
 
 <style scoped>
 .table {
-  width: 80%;
+  width: 100%;
   table-layout: auto;
   justify-self: center;
   text-align: center;
@@ -99,5 +97,34 @@ onMounted(() => {
   background-color: #EED19C;
   border: 0;
   color: #5C4033;
+}
+.add-stock-area {
+    display: block;
+    .col:not(:last-child){
+      margin-bottom: 10px;
+    }
+    .col:last-child {
+      display: flex;
+      justify-content: center;
+    }
+  }
+@media screen and (min-width: 768px) and (max-width: 1024px) {
+  .add-stock-area {
+    display: flex;
+    .col:last-child {
+      display: block;
+    }
+  }
+}
+@media screen and (min-width: 1025px) {
+  .add-stock-area {
+    display: flex;
+    .col:last-child {
+      display: block;
+    }
+  }
+  .table{
+    width: 80%;
+  }
 }
 </style>

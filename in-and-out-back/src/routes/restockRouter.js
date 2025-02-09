288ccf8cfb70195012bb298548929b18fb7a72d0
h-e-l-e-n <leeleilei07@gmail.com>
2025-02-09 23:27:51 +0800

    feat: can successfully upload image to S3

--- in-and-out-back/src/routes/restockRouter.js
+++ in-and-out-back/src/routes/restockRouter.js
@@ -18,22 +18,21 @@ router.get('/detail', async (req, res) => {
 
 
 router.post('/addstock', upload.single('file'), async (req, res, next) => {
-    let imageUrl = null; 
+    let imageUrl = null;     
 
-    // 如果有上傳檔案，則處理 S3 上傳
     if (req.file) {
-        imageUrl = uploadImage(req.file)
+        const response = await uploadImage(req.file)     
+
+        imageUrl = response        
     }
 
-    try {
+    try {        
         const { product_name, amount, purchase_cost, expire_date, product_source, purchase_order_number } = req.body;
 
-        // 檢查是否缺少必要欄位
         if (!product_name || !amount || !purchase_cost || !expire_date || !product_source || !purchase_order_number) {
             return res.status(400).json({ error: "請提供完整的商品資訊" });
         }
 
-        // 建立資料
         const response = await prisma.restocks.create({
             data: {
                 product_name,
@@ -43,19 +42,19 @@ router.post('/addstock', upload.single('file'), async (req, res, next) => {
                 product_source,
                 purchase_order_number,
                 build_time: new Date(),
-                image_url: imageUrl, 
+                stock_image: imageUrl, 
             }
         });
 
         res.status(201).json({
             message: "資料建立成功",
             status: 201,
-            data: response, // 回傳 Prisma 建立的資料
+            data: response, 
         });
 
     } catch (err) {
         console.error(err);
-        next(err); // 傳遞錯誤給 Express 的錯誤處理中介軟體
+        next(err); 
     }
 });
 
--- in-and-out-back/src/utils/s3Upload.js
+++ in-and-out-back/src/utils/s3Upload.js
@@ -1,12 +1,8 @@
-import express from 'express'
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'; 
 import 'dotenv/config'
 import multer from 'multer';
 
-const router = express.Router();
 
-
-// 從環境變數中取得 AWS 設定
 const {
     AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY,
@@ -27,7 +23,7 @@ const s3Client = new S3Client({
 export const upload = multer({
   storage: multer.memoryStorage(), // 使用記憶體儲存，檔案將保存在 RAM 中
     fileFilter: function (req, file, cb) {
-    // 驗證檔案類型，只接受 jpg 和 png 格式
+    // 驗證檔案類型
     if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
         return cb(new Error('Only jpg and png formats are allowed!'), false);
     }
@@ -35,20 +31,19 @@ export const upload = multer({
     },
 });
 
-      // 建立並發送 PutObjectCommand
-      // PutObjectCommand 用於將檔案上傳到 Amazon Simple Storage Service (Amazon S3) 的儲存桶（bucket）。
-export async function uploadImage(req) {
-    const key = Date.now().toString() + '-' + req.file.originalname; // 生成檔案名稱
+// 建立並發送 PutObjectCommand
+// PutObjectCommand 用於將檔案上傳到 Amazon Simple Storage Service (Amazon S3) 的儲存桶（bucket）。
+export async function uploadImage(req) {    
+    const key = Date.now().toString() + '-' + req.originalname;
 
     const command = new PutObjectCommand({
         Bucket: BUCKET_NAME,
         Key: key,
-        Body: req.file.buffer,
-        ContentType: req.file.mimetype,
+        Body: req.buffer,
+        ContentType: req.mimetype,
     });
-    await s3Client.send(command); // 發送命令
+    await s3Client.send(command); 
 
-    // 創建 S3 的 URL
     const imageUrl = `https://${BUCKET_NAME}.s3.${S3_BUCKET_REGION}.amazonaws.com/${key}`;
     return imageUrl
 
